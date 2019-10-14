9545ddb54f4c2aa1d4dbed74f795b70f20034429
Wayne <andrew.wayne@ccac.org>
2019-10-14 10:36:20 -0400

    Moved projects

--- Loopingproject/src/triangle/triangle.java
+++ Loopingproject/src/triangle/triangle.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package triangle;
+
 import java.util.Scanner;
 
 /**
@@ -12,43 +13,42 @@ import java.util.Scanner;
  */
 public class triangle {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int height;
         int length;
         int width;
         int position = 0;
-        int hpos = 1;
-        int blank =0; 
+        int hpos = 0;
+        int blank = 0;
         String symbol = "*";
+        String blank = " ";
 
-         System.out.println("Hello! Let's make an arch.");
+        System.out.println("Hello! Let's make an arch.");
         System.out.println("What should be the height of our arch?");
         height = input.nextInt();
         System.out.println("What about the length?");
         length = input.nextInt();
         System.out.println("And what about the width of our bases?");
         width = input.nextInt();
-        int arch=height*(2/3);
-        int center= length/2;
+        int arch = height * (2 / 3);
+        int center = length / 2;
         //Gather input for the structure
-        
-        while (hpos <= height) {
-            while (arch<hpos){
-            while (position < length) {
-                System.out.print(symbol);
-                position = position + 1;
+
+        while (hpos < height) {
+            while (arch < hpos) {
+                while (position < length) {
+                    System.out.print(symbol);
+                    position = position + 1;
                 }
-            System.out.println();
-            hpos=hpos+1;
-            position= 0;
-            }
-            
+                System.out.println();
+                hpos = hpos + 1;
+                position = 0;
+            }//end of top portion
+                
         }
-        
+
         System.out.println("Here is your box.");
-        }
-
     }
-    
 
+}
